0f541a78915134aeee71bb8bd61a36ae6c4c498b
jianggc <jianggaocheng@hotmail.com>
2017-09-22 10:45:30 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 
 /**
  * @namespace Tutu
@@ -11,7 +14,12 @@ exports.Logger = require('./tutu-logger');
 
 exports.Template = require('./lib/template');
 
+// set express middleware
 let app = express();
+app.use(bodyParser.json());
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 exports.public = require('./lib/public')(app);
 exports.app = app;
